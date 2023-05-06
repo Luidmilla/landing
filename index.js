b83963528903fb1c1c434e0bd13fb292e289e797
Luidmilla <mila291197@mail.ru>
2023-05-07 00:04:53 +0300

    итог

---
+++ index.js
@@ -0,0 +1,26 @@
+// Найти нужные узлы
+
+const popupButton = document.querySelector('.popup-menu__buton');
+const popupWrapper = document.querySelector('.popup-menu');
+const popupMenu = document.querySelector('.popup-menu__list');
+
+// Функция клика по кнопке
+
+popupButton.addEventListener('click' ,  handleMenu);
+
+function handleMenu(){
+    popupMenu.classList.toggle('hide-popup');
+}
+
+document.addEventListener('click', hidePopup);
+
+function hidePopup(el){
+    let targetInside = popupWrapper.contains(el.target);
+
+    if(!targetInside){
+        popupMenu.classList.add('hide-popup');
+    }
+    else{
+        return;
+    }
+}
